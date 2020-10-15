84cdfef99124acf089c9e3995c8404cac93f16be
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2020-10-16 02:56:51 +0530

    fixed failing cases

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 (function () {
 	'use strict';
-
+  
 	const fs = require('fs');
 	const elasticsearch = require('elasticsearch');
 	const esClient = new elasticsearch.Client({
-		host: '127.0.0.1:9200',
-		log: 'error'
+	  host: '127.0.0.1:9200',
+	  log: 'error'
 	});
-
+  
 	const bulkIndex = function bulkIndex(index, type, data) {
-		let bulkBody = [];
-		
-		data.forEach(item => {
-			bulkBody.push({
-				index: {
-					_index: index,
-					_type: type,
-					_id: item.id
-				}
-			});
-
-			bulkBody.push(item);
+	  let bulkBody = [];
+  
+	  data.forEach(item => {
+		bulkBody.push({
+		  index: {
+			_index: index,
+			_type: type,
+			_id: item.id
+		  }
 		});
-		
-		esClient.bulk({body: bulkBody})
-		.then(response => {
-			let errorCount = 0;
-			response.items.forEach(item => {
-				if (item.index && item.index.error) {
-					console.log(++errorCount, item.index.error);
-				}
-			});
-			console.log(`Successfully indexed ${data.length - errorCount} out of ${data.length} items`);
-		})
-		.catch(console.err);
+  
+		bulkBody.push(item);
+	  });
+  
+	  esClient.bulk({body: bulkBody})
+	  .then(response => {
+		let errorCount = 0;
+		response.items.forEach(item => {
+		  if (item.index && item.index.error) {
+			console.log(++errorCount, item.index.error);
+		  }
+		});
+		console.log(`Successfully indexed ${data.length - errorCount} out of ${data.length} items`);
+	  })
+	  .catch(console.err);
 	};
-
+  
 	// only for testing purposes
 	// all calls should be initiated through the module
 	const test = function test() {
-		const articlesRaw = fs.readFileSync('data.json');
-		const articles = JSON.parse(articlesRaw);
-		console.log(`${articles.length} items parsed from data file`);
-		bulkIndex('library', 'article', articles);
+	  const articlesRaw = fs.readFileSync('data.json');
+	  const articles = JSON.parse(articlesRaw);
+	  console.log(`${articles.length} items parsed from data file`);
+	  bulkIndex('library', 'article', articles);
 	};
-
+  
 	test();
-
+  
 	module.exports = {
-		bulkIndex
+	  bulkIndex
 	};
-} ());
+  } ());
+  
